f86d9e0658be4237bf087e621e5b15742cca09d9
FereroPayne <160160367+FereroPayne@users.noreply.github.com>
2026-03-13 16:40:18 +0100

    Add function declarations and operator overload
    Added function declarations and an overloaded operator<< for Lang.

--- LangParser.cpp
+++ LangParser.cpp
@@ -1,1 +1,19 @@
-#pragma once
+#include<iostream>
+#include "Lang.h"
+
+
+
+std::string tri(std::string key, std::vector<int> values = std::vector<int>());
+std::string trs(std::string key, std::vector<std::string> values = std::vector<std::string>());
+
+
+std::ostream& operator=<<(std::ostream& os, const & l) {
+    os << lang << std::endl;
+    return os;
+
+  std::string input="";  std::getline(std::cin, input);  
+
+
+
+}
+
