cc256034805f2d45baeb8febc2d78e1aa94739a0
FereroPayne <160160367+FereroPayne@users.noreply.github.com>
2026-03-13 16:39:03 +0100

    Add Lang.cpp with language functions and operator overload

---
+++ Lang.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include "Lang.h"
+
+
+
+std::string tri(std::string key, std::vector<int> values = std::vector<int>());
+std::string trs(std::string key, std::vector<std::string> values = std::vector<std::string>());
+
+
+std::ostream& operator=<<(std::ostream& os, const & l) {
+    os << lang << std::endl;
+    return os;
+
+  std::string input="";  std::getline(std::cin, input);  
+
+
+
+}
